8d3d6840fab49b69f5755509b8201f1894572f42
Mallikharjuna71 <meka.mallikharjunareddy@diggibyte.com>
2024-09-30 20:06:11 +0530

    finding happiness of an array

--- src/assignment_15/util.py
+++ src/assignment_15/util.py
@@ -1,3 +1,4 @@
+
 def no_idea(array, a, b):
     total = 0
     for i in array:
--- test/assignment_15.py
+++ test/assignment_15.py
@@ -1,5 +1,6 @@
 import unittest
+
 from python_assignment.src.assignment_15.util import no_idea
 class Test(unittest.TestCase):
-    def test_email(self):
+    def test_total(self):
         self.assertEqual(no_idea([1, 5, 3], {3, 1}, {5, 7}), 1)
